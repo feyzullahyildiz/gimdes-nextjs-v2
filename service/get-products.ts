5a13de0621bd7d4f9160009898def738a1c3bb8b
A. Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2025-06-02 02:34:10 +0300

    _tr_sort _tr_order getirildi

--- service/get-products.ts
+++ service/get-products.ts
@@ -3,8 +3,10 @@ import { ISertifikaItem } from "@/types/typesense/ApiRes";
 
 export async function getProductsByCategory(categoryId: string) {
   const query = new URLSearchParams();
-  query.set("_sort", "MarkaAdi,FirmaAdi");
-  // query.set("_order", "asc");
+  // query.set("_sort", "MarkaAdi,FirmaAdi");
+  // query.set("_order", "desc,desc");
+  query.set("_tr_sort", "MarkaAdi,FirmaAdi");
+  // query.set("_tr_order", "desc,desc");
   query.set("KategoriId", categoryId);
   const response = await fetch(
     `${process.env.JSON_SERVER_API}/api/latest/sertifikalar?${query.toString()}`,
